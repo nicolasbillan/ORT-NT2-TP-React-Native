baeeb6043767b3132ed197ec9290cb3d79d1f35c
Nacho Giss <giss.ignacio@gmail.com>
2021-11-15 21:07:36 -0300

    retrieve and store exercises at start

--- fitness-app/App.js
+++ fitness-app/App.js
@@ -24,9 +24,40 @@ export default function App() {
           const jsonData = JSON.parse(data)
           dispatch({'type':'LOGIN', payload:{'email':jsonData.email, 'token':jsonData.token}})
         }
-           
+
         })
-      },[])
+        const exercises = AsyncStorage.getItem('exercises').then(
+        (exercises) => {
+        if (!exercises)
+          retrieveExercises()
+        })
+    },[])
+
+      function retrieveExercises() {
+
+        // TODO: put on a helper method.
+        const headers = new Headers()
+        headers.append("Content-type", "application/json")
+        headers.append("Access-Control-Allow-Origin", "http://localhost:3000/api/ejercicios")
+        headers.append("Access-Control-Allow-Methods", "GET")
+        headers.append("Access-Control-Allow-Headers", "Content-Type")
+
+        const requestOptions = {
+            method:"GET",
+            headers:headers
+          }
+
+        return fetch('http://localhost:3000/api/ejercicios', requestOptions )
+        .then( resp => {
+            if (!resp.ok)
+                throw Error("Error:" + resp.statusText)
+            return resp.json()
+        })
+        .then( jsonExercises => {
+           dispatch({'type':'STORE_EXERCISES', payload:{ 'exercises':jsonExercises }})
+        })
+        .catch( error => console.log("Error:" + error))
+    }
 
       return (
         <GlobalContext.Provider value={{state,dispatch}}>
